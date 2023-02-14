3f9a0886bf532e6bb29ed78716f629c868fb812c
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-02-14 16:00:38 -0500

    total recall day 1

---
+++ Module_1/total_recall/index.js
@@ -0,0 +1,88 @@
+// Example of What your JS File in VS Code should look like
+
+
+/*
+	Remember!!! Once you’ve completed a problem,
+	COMMENT OUT YOUR WORK before moving on to the next one.
+	This makes it easier to read and debug the current 
+	Solution you are working on.
+*/
+
+
+// I. Variables & Data Types
+// A. Q + A
+// 1. Using a = operator
+// 2. We reference the variable 
+
+// B. Strings
+// C. Booleans
+// D. The Farm
+// E. Driver's Ed
+//_____________________________________________________________
+
+
+
+
+
+
+// II. Loops
+// A. The Basics
+// B. Get even
+// C. Give me Five
+// D. Savings account
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// III. Arrays & Control Flow
+// A. Talk about it:
+// B. Easy Does It
+// C. Accessing elements
+// D. Change values
+// E. Mix It Up
+// F. Biggie Smalls
+// G. Monkey in the Middle
+// H. What's in Your Closet?
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// IV. Functions
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+// H.
+//_____________________________________________________________
+
+
+
+
+
+
+// V. Objects
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+//_____________________________________________________________
+
+
+// Extra
+
