201c4789d5872f48c9304ea47ae5b4efc0705b0b
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-02-16 22:32:28 -0500

    mdian

--- Module_1/total_recall/d2_total_recall.js
+++ Module_1/total_recall/d2_total_recall.js
@@ -1,13 +1,19 @@
 ////////////////////////////////
 //*  Easy Going
 ////////////////////////////////
-
+// Write a for loop that will log the numbers 1 through 20.
+for (let i = 1; i <= 20; i++) {
+    // console.log(i);
+}
 
 
 ////////////////////////////////
 //* Get Even
 ////////////////////////////////
-
+// Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i+=2) {
+    // console.log(i);
+}
 
 ////////////////////////////////
 //* Fizz Buzz
@@ -42,3 +48,27 @@
 ////////////////////////////////
 //*  Find the Median
 ////////////////////////////////
+// Find the median number in the following nums array, then console.log that number.
+// hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+// sort array in place
+nums.sort();
+const middle = Math.floor(nums.length / 2)
+
+console.log(nums[middle]);
+//______
+function findMedian(arrayOfNums) {
+    arrayOfNums.sort((a, b) => a-b); // sort the array
+    const middle = Math.floor(arrayOfNums.length / 2);
+
+    // if array is odd, return the middle value
+    if (arrayOfNums.length % 2 === 1) {
+        return arrayOfNums[middle];
+    } else {
+        // if array is even add the  middle and the value before divided by 2
+        return ( arrayOfNums[middle - 1] + arrayOfNums[middle] ) / 2;
+    }
+}
+
+console.log(findMedian([2, 1, 3, 4]));
---
+++ Module_1/total_recall/day3_total_recall.js
@@ -0,0 +1,62 @@
+
+// Creating Classes & Factories
+
+class Hamster {
+    constructor(name){
+      this.owner = ''
+      this.name = name
+      this.price = 15
+    }
+    wheelRun(){
+      console.log('squeak squeak')
+    }
+    eatFood(){
+      console.log('nibble nibble')
+    }
+    getPrice(){
+      return this.price
+    }
+  }
+
+  class Person {
+    constructor(name){
+      this.name = name
+      this.age = 0
+      this.height = 0
+      this.weight = 0
+      this.mood = 0
+      this.hamsters = []
+      this.bankAccount = 0
+    }
+    getName(){
+      return this.name
+    }
+    getAge(){
+      return this.age
+    }
+    getWeight(){
+      return this.weight
+    }
+    greet(){
+      console.log(`I am ${this.name} hello`)
+    }
+    eat(){
+      this.weight++
+      this.mood++
+    }
+    exercise(){
+      this.weight--
+    }
+    ageUp(){
+      this.age++
+      this.height++
+      this.weight++
+      this.mood--
+      this.bankAccount+=10
+    }
+    buyHamster(hamster){
+      this.hamsters.push(hamster)
+      this.mood+=10
+      this.bankAccount-=hamster.getPrice()
+    }
+  }
